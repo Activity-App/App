12cab28ae07798381445640013b2a0e1144d6b99
wfltaylor <wfltaylor@gmail.com>
2020-07-10 13:22:26 +1000

    Fixed workflow triggers

---
+++ Project SF/CloudKitStore.swift
@@ -0,0 +1,20 @@
+//
+//  CloudKitStore.swift
+//  Project SF
+//
+//  Created by William Taylor on 10/7/20.
+//
+
+import Foundation
+import CloudKit
+
+class CloudKitStore {
+    
+    //MARK: Singleton
+    
+    static let shared = CloudKitStore()
+    private init() {}
+    
+    //MARK: 
+    
+}
