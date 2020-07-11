443307e0b4ea672cd265e9b56798460cc0cf3071
Roman Esin <esinromanswift@gmail.com>
2020-07-11 16:52:23 +1000

    Update SettingsView.swift

--- Project SF/Views/Tabs/SettingsView.swift
+++ Project SF/Views/Tabs/SettingsView.swift
@@ -25,7 +25,6 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-
                 Section(header: Text("General"), content: {
                     SettingsLabel(title: "Profile",
                                   systemName: "person.crop.circle",
