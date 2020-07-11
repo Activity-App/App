5ac2656088741e692b7f75010a00026dc6927b98
wfltaylor <wfltaylor@gmail.com>
2020-07-11 21:07:05 +1000

    Split tests into different classes

--- Project SFTests/ProjectSFTests.swift
+++ Project SFTests/ProjectSFTests.swift
@@ -1,5 +1,5 @@
 //
-//  Project_SFTests.swift
+//  NetworkManagerTests.swift
 //  Project SFTests
 //
 //  Created by William Taylor on 10/7/20.
@@ -9,25 +9,7 @@ import XCTest
 import Combine
 @testable import Project_SF
 
-class ProjectSFTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    // MARK: Utilities
-
-    func testRangeConversion() throws {
-        XCTAssertEqual(1.0.convert(fromRange: 0...10, toRange: 0...1), 0.1)
-        XCTAssertEqual(0.25.convert(fromRange: 0...1, toRange: 0...100), 25.0)
-        XCTAssertEqual(0.1.convert(fromRange: 0...1, toRange: 0...10), 1.0)
-        XCTAssertEqual(0.convert(fromRange: 0...5, toRange: 0...10), 0)
-        XCTAssertEqual(5.convert(fromRange: 0...5, toRange: 0...10), 10)
-    }
+class NetworkManagerTests: XCTestCase {
     
     // MARK: Network Manager
     
@@ -53,7 +35,7 @@ class ProjectSFTests: XCTestCase {
     
     func testNetworkManagerErrorRequest() throws {
         let expect = XCTestExpectation()
-
+        
         let mock = URLSessionMock()
         mock.error = FakeError()
         
@@ -104,7 +86,7 @@ class ProjectSFTests: XCTestCase {
         
         let networkManager = NetworkManager(urlSession: mock)
 
-        networkManager.request(URL(string: "fake"), decode: SampleDataStruct.self) { result in
+        networkManager.request(URL(string: "fake"), decode: SampleData.self) { result in
             switch result {
             case .success(let decodedObject):
                 XCTAssertEqual(decodedObject.name, "test")
@@ -208,33 +190,6 @@ class ProjectSFTests: XCTestCase {
         wait(for: expectations, timeout: 2)
         cancellables
             .forEach { $0.cancel() }
-    }
-    
-    // MARK: Performance
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    // MARK: Fake Error
-    
-    private struct FakeError: Error {
-        
-        let uuid = UUID()
-        
-    }
-    
-    // MARK: Sample Data Struct
-    
-    private struct SampleDataStruct: Codable {
-        
-        let name: String
-        
-        let count: Int
-        
     }
 
 }
