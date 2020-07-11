1e51893ac8f59d467f74d3e4f1482798a9aec2b5
Roman Esin <esinromanswift@gmail.com>
2020-07-11 11:08:03 +1000

    Added RoundedButton.

---
+++ Project SF/Views/RoundedButton.swift
@@ -0,0 +1,38 @@
+//
+//  RoundedButton.swift
+//  Project SF
+//
+//  Created by Roman Esin on 11.07.2020.
+//
+
+import SwiftUI
+
+struct RoundedButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(.title2))
+                .fontWeight(.medium)
+                .padding(12)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(16)
+                .padding()
+        }
+    }
+
+    init(_ title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+}
+
+struct RoundedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedButton()
+    }
+}
