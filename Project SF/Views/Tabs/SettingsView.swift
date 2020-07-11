8244a2ceb67cf82a84d7e4d5dec024224552d2f9
Roman Esin <esinromanswift@gmail.com>
2020-07-11 16:36:54 +1000

    Add SettingsLabel.

--- Project SF/Views/Tabs/SettingsView.swift
+++ Project SF/Views/Tabs/SettingsView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct SettingsLabel<Destination: View>: View {
+    let title: String
+    let systemName: String
+    let destination: Destination
+
+    var body: some View {
+        NavigationLink(
+            destination: destination,
+            label: {
+                Label(title, systemImage: systemName)
+            })
+    }
+}
+
 struct SettingsView: View {
     var body: some View {
         NavigationView {
