49d4ea9c5d09427b35992aa7de508428caf9b5ff
Priva28 <priva2804@gmail.com>
2020-07-12 15:42:59 +1000

    fix

--- Project SF/Support/ProjectSFApp.swift
+++ Project SF/Support/ProjectSFApp.swift
@@ -4,16 +4,32 @@
 //
 //  Created by Christian Privitelli on 10/7/20.
 //
-
 import SwiftUI
 
 struct ProjectSFApp: App {
     
     //let cloudKitStore = CloudKitStore.shared
+    @AppStorage("showOnboarding") var showOnboarding = true
+    @StateObject var healthKit = HealthKitController()
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showOnboarding {
+                OnboardingView(showOnboarding: $showOnboarding)
+                    .environmentObject(healthKit)
+            } else {
+                ContentView()
+                    .environmentObject(healthKit)
+                    .onAppear {
+                        healthKit.authorizeHealthKit {
+                            if healthKit.authorizationState == .granted {
+                                healthKit.updateTodaysActivityData()
+                            }
+                        }
+                    }
+                    .transition(AnyTransition.opacity.animation(.linear(duration: 1)))
+            }
         }
+        
     }
 }
