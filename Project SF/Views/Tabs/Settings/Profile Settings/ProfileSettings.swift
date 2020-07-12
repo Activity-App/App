b64bc1ee5c1d7351c02639c1c93e8f154ce0a53b
wfltaylor <wfltaylor@gmail.com>
2020-07-12 15:39:15 +1000

    Update ProfileSettings.swift

--- Project SF/Views/Tabs/Settings/Profile Settings/ProfileSettings.swift
+++ Project SF/Views/Tabs/Settings/Profile Settings/ProfileSettings.swift
@@ -11,77 +11,84 @@ import Combine
 struct ProfileSettings: View {
     
     // MARK: Properties
-
+    
     @AppStorage("phone-number") var phoneNumber = "+7 (914) 690 52-28"
     
     @State var nickname = ""
-
+    
     @State var isShowingAlert = false
     @StateObject var controller = ProfileSettingsController()
     
     // MARK: View
-
+    
     var body: some View {
-        ScrollView {
-            switch controller.userController.state {
-            case .loading:
-                ProgressView()
-            case .user(_):
-                if controller.userController.isSyncing {
-                    ProgressView {
-                        Text("Syncing")
-                    }
-                }
-                Button(action: {
-                    // TODO: Present image search through the gallery.
-                }, label: {
-                    Image(systemName: "person.crop.circle.badge.plus")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                })
-                .frame(height: 100)
-                .padding(.vertical)
-
-                GroupBox {
-                    HStack {
-                        TextField("Enter your name", text: $controller.nicknameText) { (didChange) in
-                            print(didChange)
-                        } onCommit: {
-                            controller.setNickname()
+        // This VStack and empty text is required to fix nav title glitching out on scroll
+        // So ScrollView isnt the topmost view.
+        // This VStack and empty text is required to fix the navigation title glitching out on scroll
+        // so ScrollView isn't the topmost view.
+        VStack(spacing: 0) {
+            Text("")
+            ScrollView {
+                switch controller.userController.state {
+                case .loading:
+                    ProgressView()
+                case .user(_):
+                    if controller.userController.isSyncing {
+                        ProgressView {
+                            Text("Syncing")
                         }
-                        .multilineTextAlignment(.leading)
-
-                        Image(systemName: "pencil")
                     }
-                    .font(.headline)
-                }
-
-                GroupBox {
-                    HStack {
-                        TextField("Enter your phone number", text: $phoneNumber) { (didChange) in
-                            print(didChange)
-                        } onCommit: {
-                            print("Commited")
+                    Button(action: {
+                        // TODO: Present image search through the gallery.
+                    }, label: {
+                        Image(systemName: "person.crop.circle.badge.plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    })
+                    .frame(height: 100)
+                    .padding(.vertical)
+                    
+                    GroupBox {
+                        HStack {
+                            TextField("Enter your name", text: $controller.nicknameText) { (didChange) in
+                                print(didChange)
+                            } onCommit: {
+                                controller.setNickname()
+                            }
+                            .multilineTextAlignment(.leading)
+                            
+                            Image(systemName: "pencil")
                         }
-                        .multilineTextAlignment(.leading)
-
-                        Image(systemName: "pencil")
+                        .font(.headline)
                     }
-                    .font(.headline)
-                }
-
-                GroupBox {
-                    HStack {
-                        Text("Enter your bio or description")
-                            .foregroundColor(Color(.tertiaryLabel))
-                        Spacer()
+                    
+                    GroupBox {
+                        HStack {
+                            TextField("Enter your phone number", text: $phoneNumber) { (didChange) in
+                                print(didChange)
+                            } onCommit: {
+                                print("Commited")
+                            }
+                            .multilineTextAlignment(.leading)
+                            
+                            Image(systemName: "pencil")
+                        }
+                        .font(.headline)
                     }
-                    TextEditor(text: $controller.bioText)
-                        .cornerRadius(8)
+                    
+                    GroupBox {
+                        HStack {
+                            Text("Enter your bio or description")
+                                .foregroundColor(Color(.tertiaryLabel))
+                            Spacer()
+                        }
+                        TextEditor(text: $controller.bioText)
+                            .cornerRadius(8)
+                    }
+                    .frame(height: 250)
+                case .failure(let error):
+                    Text("Error: \(error.localizedDescription)")
                 }
-                .frame(height: 250)
-            case .failure(let error):
-                Text("Error: \(error.localizedDescription)")
             }
         }
         .padding(.horizontal)
