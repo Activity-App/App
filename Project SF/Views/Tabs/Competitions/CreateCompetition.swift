ab3b79de83ee67daf08bae8927f26489bd07834c
Roman Esin <esinromanswift@gmail.com>
2020-07-12 22:35:35 +1000

    Update CreateCompetition.swift

--- Project SF/Views/Tabs/Competitions/CreateCompetition.swift
+++ Project SF/Views/Tabs/Competitions/CreateCompetition.swift
@@ -9,29 +9,50 @@ import SwiftUI
 
 struct CreateCompetition: View {
 
+    @Environment(\.presentationMode) var presentationMode
     @State var competitionName = ""
     @State var competitionEndDate = Date() + 60 * 60 * 24
+    @State var pickedDate = 0
 
     var body: some View {
-        VStack {
-            GroupBox {
-                TextField("Competition name", text: $competitionName) { _ in
+//        ScrollView {
+            VStack {
+                GroupBox {
+                    TextField("Competition name", text: $competitionName) { _ in
 
                 } onCommit: {
+                    } onCommit: {
 
+                    }
                 }
+
+                GroupBox(label: Text("Pick competition end date").foregroundColor(Color(UIColor.tertiaryLabel)),
+                         content: {
+                    Picker("Pick competition end date", selection: $pickedDate) {
+                        Text("1 Day").tag(0)
+                        Text("7 Days").tag(1)
+                        Text("1 Month").tag(2)
+                        Text("Custom").tag(3)
+                    }.pickerStyle(SegmentedPickerStyle())
+
+                    // TODO: Add animation to this thing
+                    if pickedDate == 3 {
+                        DatePicker("", selection: $competitionEndDate)
+                            .frame(maxHeight: 70)
+//                            .padding(.horizontal, 16)
+//                            .datePickerStyle(GraphicalDatePickerStyle())
+                    }
+                })
+
+                Spacer()
+
+                RoundedButton("Start the competition") {
+                    guard !competitionName.isEmpty else { return }
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .disabled(competitionName.isEmpty)
             }
-
-            GroupBox(label: Text("Competition end date").foregroundColor(Color(UIColor.tertiaryLabel)), content: {
-                DatePicker("Competition end date", selection: $competitionEndDate, in: Date()...)
-                    .datePickerStyle(GraphicalDatePickerStyle())
-            })
-
-            RoundedButton("Start the competition") {
-
-            }
-        }
-        .padding(.horizontal)
+            .padding(.horizontal)
         .navigationTitle("Create competition")
     }
 }
