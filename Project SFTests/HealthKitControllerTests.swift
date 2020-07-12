3886437698f6d0051f2e71e15aaa77657a75df6b
wfltaylor <wfltaylor@gmail.com>
2020-07-12 10:31:37 +1000

    Simplifiy tests

--- Project SFTests/HealthKitControllerTests.swift
+++ Project SFTests/HealthKitControllerTests.swift
@@ -58,7 +58,7 @@ class HealthKitControllerTests: XCTestCase {
     }
 
     // MARK: - Test Update Activity Data
-    // swiftlint:disable function_body_length
+    
     func testHealthKitControllerUpdateActivityData() throws {
         let mock = HKHealthStoreMock()
         let fakeResult = HKActivitySummaryMock()
@@ -75,48 +75,19 @@ class HealthKitControllerTests: XCTestCase {
         
         let controller = HealthKitController(healthStore: mock)
         
-        var cancellables = [AnyCancellable]()
-        var expectations = [XCTestExpectation]()
-        
-        func handle(expectedResult: Double, publisher: Published<Double>.Publisher, description: String = "") {
-            let expectation = XCTestExpectation(description: description)
-            let cancellable = publisher
-                .filter {
-                    print(description, $0, expectedResult)
-                    return $0 == expectedResult
-                }
-                .sink { _ in
-                    expectation.fulfill()
-                }
-            
-            cancellables.append(cancellable)
-            expectations.append(expectation)
-        }
+        let expectation = XCTestExpectation()
 
         controller.updateTodaysActivityData {
-            handle(expectedResult: 100.0,
-                   publisher: controller.$moveCurrent,
-                   description: "Move value is not set correctly")
-            handle(expectedResult: 200.0,
-                   publisher: controller.$moveGoal,
-                   description: "Move goal is not set correctly")
-            handle(expectedResult: 50.0,
-                   publisher: controller.$exerciseCurrent,
-                   description: "Exercise value is not set correctly")
-            handle(expectedResult: 60.0,
-                   publisher: controller.$exerciseGoal,
-                   description: "Exercise goal is not set correctly")
-            handle(expectedResult: 50.0,
-                   publisher: controller.$standCurrent,
-                   description: "Stand value is not set correctly")
-            handle(expectedResult: 60.0,
-                   publisher: controller.$standGoal,
-                   description: "Stand goal is not set correctly")
+            XCTAssertEqual(controller.moveCurrent, 100, "Move value is not set correctly")
+            XCTAssertEqual(controller.moveGoal, 200, "Move goal is not set correctly")
+            XCTAssertEqual(controller.exerciseCurrent, 50, "Exercise value is not set correctly")
+            XCTAssertEqual(controller.exerciseGoal, 60, "Exercise goal is not set correctly")
+            XCTAssertEqual(controller.standCurrent, 50, "Stand value is not set correctly")
+            XCTAssertEqual(controller.standGoal, 60, "Stand goal is not set correctly")
+            expectation.fulfill()
         }
         
-        wait(for: expectations, timeout: 5)
-        cancellables
-            .forEach { $0.cancel() }
+        wait(for: [expectation], timeout: 5)
     }
 
 }
