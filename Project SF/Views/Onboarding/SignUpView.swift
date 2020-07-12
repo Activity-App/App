592d09e6e45db965a773a8e5bf8bfc5151ae0652
Roman Esin <esinromanswift@gmail.com>
2020-07-12 18:15:51 +1000

    Update SignUpView.swift

--- Project SF/Views/Onboarding/SignUpView.swift
+++ Project SF/Views/Onboarding/SignUpView.swift
@@ -72,8 +72,8 @@ struct SignUpView: View {
         var index = numbers.startIndex // numbers iterator
 
         // iterate over the mask characters until the iterator of numbers ends
-        for ch in mask where index < numbers.endIndex {
-            if ch == "X" {
+        for char in mask where index < numbers.endIndex {
+            if char == "X" {
                 // mask requires a number in this place, so take the next one
                 result.append(numbers[index])
 
@@ -81,7 +81,7 @@ struct SignUpView: View {
                 index = numbers.index(after: index)
 
             } else {
-                result.append(ch) // just append a mask character
+                result.append(char) // just append a mask character
             }
         }
         return result
