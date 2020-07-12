2cc58aed526b277879c96408d82343576c94f99e
Roman Esin <esinromanswift@gmail.com>
2020-07-12 23:11:15 +1000

    Fix indentation.

--- Project SF/Views/Tabs/Competitions/CreateCompetition.swift
+++ Project SF/Views/Tabs/Competitions/CreateCompetition.swift
@@ -16,58 +16,57 @@ struct CreateCompetition: View {
     @State var invitedFriends: [String] = []
 
     var body: some View {
-//        ScrollView {
-            VStack {
-                GroupBox {
-                    TextField("Enter Competition Name", text: $competitionName) { _ in
+        VStack {
+            GroupBox {
+                TextField("Enter Competition Name", text: $competitionName) { _ in
 
-                    } onCommit: {
+                } onCommit: {
 
-                    }
                 }
-
-                GroupBox(label: Text("Pick competition end date").foregroundColor(Color(UIColor.tertiaryLabel)),
-                         content: {
-                    Picker("Pick competition end date", selection: $pickedDate) {
-                        Text("1 Day").tag(0)
-                        Text("7 Days").tag(1)
-                        Text("1 Month").tag(2)
-                        Text("Custom").tag(3)
-                    }.pickerStyle(SegmentedPickerStyle())
-
-                    // TODO: Add animation to this thing
-                    if pickedDate == 3 {
-                        DatePicker("", selection: $competitionEndDate)
-                            .frame(maxHeight: 50)
-                    }
-                })
-
-                GroupBox {
-                    Button("Invite friends") {
-                        // TODO: Hanlde invite friends
-                        invitedFriends.append("nmsdnjosadn\(Int.random(in: 0...100))")
-                    }
-                    .frame(maxWidth: .infinity)
-
-                    if !invitedFriends.isEmpty {
-                        List(invitedFriends.indices) { index in
-                            Section {
-                                Text(invitedFriends[index])
-                            }
-                        }
-                        .listStyle(InsetGroupedListStyle())
-                    }
-                }
-
-                Spacer()
-
-                RoundedButton("Start the competition") {
-                    guard !competitionName.isEmpty else { return }
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .disabled(competitionName.isEmpty)
             }
-            .padding(.horizontal)
+
+            GroupBox(label: Text("Pick competition end date").foregroundColor(Color(UIColor.tertiaryLabel)),
+                     content: {
+                        Picker("Pick competition end date", selection: $pickedDate) {
+                            Text("1 Day").tag(0)
+                            Text("7 Days").tag(1)
+                            Text("1 Month").tag(2)
+                            Text("Custom").tag(3)
+                        }.pickerStyle(SegmentedPickerStyle())
+
+                        // TODO: Add animation to this thing
+                        if pickedDate == 3 {
+                            DatePicker("", selection: $competitionEndDate)
+                                .frame(maxHeight: 50)
+                        }
+                     })
+
+            GroupBox {
+                Button("Invite friends") {
+                    // TODO: Hanlde invite friends
+                    invitedFriends.append("nmsdnjosadn\(Int.random(in: 0...100))")
+                }
+                .frame(maxWidth: .infinity)
+
+                if !invitedFriends.isEmpty {
+                    List(invitedFriends.indices) { index in
+                        Section {
+                            Text(invitedFriends[index])
+                        }
+                    }
+                    .listStyle(InsetGroupedListStyle())
+                }
+            }
+
+            Spacer()
+
+            RoundedButton("Start the competition") {
+                guard !competitionName.isEmpty else { return }
+                presentationMode.wrappedValue.dismiss()
+            }
+            .disabled(competitionName.isEmpty)
+        }
+        .padding(.horizontal)
         .navigationTitle("Create competition")
     }
 }
