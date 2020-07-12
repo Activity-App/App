2da0d1106100c19dd250541d689f5fdf1954bd20
Priva28 <priva2804@gmail.com>
2020-07-12 23:36:06 +1000

    Remove unneeded

--- Project SF/Views/Tabs/Competitions/CreateCompetition.swift
+++ Project SF/Views/Tabs/Competitions/CreateCompetition.swift
@@ -16,85 +16,80 @@ struct CreateCompetition: View {
     @State var invitedFriends: [String] = []
 
     var body: some View {
-//        ScrollView {
-            VStack {
-                RoundedTextField("Competition Name", text: $competitionName) { _ in
-
-                } onCommit: {
-
-                }
+        VStack {
+            RoundedTextField("Competition Name", text: $competitionName)
                 .padding(.vertical, 8)
 
-                Group {
-                    VStack {
-                        Text("End Date")
-                            .foregroundColor(Color(.tertiaryLabel))
-                            .bold()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Picker("Pick competition end date", selection: $pickedDate) {
-                            Text("1 Day").tag(0)
-                            Text("7 Days").tag(1)
-                            Text("1 Month").tag(2)
-                            Text("Custom").tag(3)
-                        }.pickerStyle(SegmentedPickerStyle())
-                        if pickedDate == 3 {
-                            DatePicker("", selection: $competitionEndDate)
-                                .frame(maxHeight: 50)
-                                .transition(
-                                    AnyTransition
-                                        .opacity.animation(.easeInOut)
-                                        .combined(with: AnyTransition
-                                                    .move(edge: .top)
-                                                    .animation(.easeInOut)))
-                                .padding(.trailing, 8)
-                        }
+            Group {
+                VStack {
+                    Text("End Date")
+                        .foregroundColor(Color(.tertiaryLabel))
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Picker("Pick competition end date", selection: $pickedDate) {
+                        Text("1 Day").tag(0)
+                        Text("7 Days").tag(1)
+                        Text("1 Month").tag(2)
+                        Text("Custom").tag(3)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    if pickedDate == 3 {
+                        DatePicker("", selection: $competitionEndDate)
+                            .frame(maxHeight: 50)
+                            .transition(
+                                AnyTransition
+                                    .opacity.animation(.easeInOut)
+                                    .combined(with: AnyTransition
+                                                .move(edge: .top)
+                                                .animation(.easeInOut)))
+                            .padding(.trailing, 8)
                     }
-                    .padding(16)
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .frame(minHeight: 50)
-                        .foregroundColor(Color(.secondarySystemBackground))
-                )
-                .animation(.easeInOut)
-                .padding(.bottom, 8)
-                
-                Group {
-                    VStack {
-                        Button("Invite friends") {
-                            // TODO: Hanlde invite friends
-                            invitedFriends.append("nmsdnjosadn\(Int.random(in: 0...100))")
-                        }
-                        .frame(maxWidth: .infinity)
-
-                        if !invitedFriends.isEmpty {
-                            List(invitedFriends.indices) { index in
-                                Section {
-                                    Text(invitedFriends[index])
-                                }
-                            }
-                            .listStyle(InsetGroupedListStyle())
-                        }
-                    }
-                    .padding(16)
-                }
-                .background(
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .frame(minHeight: 50)
-                        .foregroundColor(Color(.secondarySystemBackground))
-                )
-                .animation(.easeInOut)
-                .padding(.bottom, 8)
-
-                Spacer()
-
-                RoundedButton("Start the competition") {
-                    guard !competitionName.isEmpty else { return }
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .disabled(competitionName.isEmpty)
+                .padding(16)
             }
-            .padding(.horizontal)
+            .background(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .frame(minHeight: 50)
+                    .foregroundColor(Color(.secondarySystemBackground))
+            )
+            .animation(.easeInOut)
+            .padding(.bottom, 8)
+            
+            Group {
+                VStack {
+                    Button("Invite friends") {
+                        // TODO: Handle invite friends
+                        invitedFriends.append("nmsdnjosadn\(Int.random(in: 0...100))")
+                    }
+                    .frame(maxWidth: .infinity)
+
+                    if !invitedFriends.isEmpty {
+                        List(invitedFriends.indices) { index in
+                            Section {
+                                Text(invitedFriends[index])
+                            }
+                        }
+                        .listStyle(InsetGroupedListStyle())
+                    }
+                }
+                .padding(16)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .frame(minHeight: 50)
+                    .foregroundColor(Color(.secondarySystemBackground))
+            )
+            .animation(.easeInOut)
+            .padding(.bottom, 8)
+
+            Spacer()
+
+            RoundedButton("Start the competition") {
+                guard !competitionName.isEmpty else { return }
+                presentationMode.wrappedValue.dismiss()
+            }
+            .disabled(competitionName.isEmpty)
+        }
+        .padding(.horizontal)
         .navigationTitle("Create competition")
     }
 }
