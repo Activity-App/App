d0870a4b3db68bfd23e0a133ba1bc255e45d1e98
Roman Esin <esinromanswift@gmail.com>
2020-07-13 11:12:01 +1000

    Fix SwiftLint

--- Project SF/Models/ActivityRings.swift
+++ Project SF/Models/ActivityRings.swift
@@ -14,4 +14,16 @@ struct ActivityRings {
     var exerciseGoal: Double
     var standCurrent: Double
     var standGoal: Double
+
+    var moveFraction: String {
+        "\(Int(moveCurrent))/\(Int(moveGoal))"
+    }
+
+    var exerciseFraction: String {
+        "\(Int(exerciseCurrent))/\(Int(exerciseGoal))"
+    }
+
+    var standFraction: String {
+        "\(Int(standCurrent))/\(Int(standGoal))"
+    }
 }
