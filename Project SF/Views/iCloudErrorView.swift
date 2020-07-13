119222ff736c5e4bb912f6cc3a1ed2f8da28738c
Roman Esin <esinromanswift@gmail.com>
2020-07-13 22:01:39 +1000

    Update iCloudErrorView.swift

--- Project SF/Views/iCloudErrorView.swift
+++ Project SF/Views/iCloudErrorView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 // swiftlint:disable type_name
 struct iCloudErrorView: View {
+    let error: String
+
     var body: some View {
         NavigationView {
-            Text("")
-                .foregroundColor(.secondary)
-                .navigationTitle("iCloud Error")
+            VStack {
+                Image(systemName: "icloud.slash.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.red)
+                    .padding(50)
+                VStack(spacing: 16) {
+                    Text("There has been an error with connection to the iCloud.")
+                    // TODO: Show different error description depending on the error
+                    Text(error)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+            }
+            .foregroundColor(.secondary)
+            .navigationTitle("iCloud Error")
         }
     }
 }
@@ -21,6 +36,6 @@ struct iCloudErrorView: View {
 // swiftlint:disable type_name
 struct iCloudErrorView_Previews: PreviewProvider {
     static var previews: some View {
-        iCloudErrorView()
+        iCloudErrorView(error: "123")
     }
 }
