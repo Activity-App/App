7bcdd2ef7e89db0f0860af794597eb2f24862a91
Roman Esin <esinromanswift@gmail.com>
2020-07-13 21:52:44 +1000

    Add iCloudErrorView.

---
+++ Project SF/Views/iCloudErrorView.swift
@@ -0,0 +1,26 @@
+//
+//  iCloudErrorView.swift
+//  Project SF
+//
+//  Created by Roman Esin on 13.07.2020.
+//
+
+import SwiftUI
+
+// swiftlint:disable type_name
+struct iCloudErrorView: View {
+    var body: some View {
+        NavigationView {
+            Text("")
+                .foregroundColor(.secondary)
+                .navigationTitle("iCloud Error")
+        }
+    }
+}
+
+// swiftlint:disable type_name
+struct iCloudErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        iCloudErrorView()
+    }
+}
