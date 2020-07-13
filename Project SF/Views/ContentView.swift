ee57d8b2f48539b52df0f700c1f560573b648da4
Roman Esin <esinromanswift@gmail.com>
2020-07-13 10:39:51 +1000

    Fix files. Add AccentColor.

--- Project SF/Views/ContentView.swift
+++ Project SF/Views/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
             ProfileView()
                 .tag(3)
         }
-        .accentColor(Color("mainAccent"))
+        .accentColor(Color.accentColor)
     }
 }
 
